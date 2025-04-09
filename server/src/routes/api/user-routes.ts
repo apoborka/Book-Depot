116ea0a2b42b6820dd7f948fe47bfb4ce216a918
apoborka <alex.poborka@gmail.com>
2025-04-08 20:02:52 -0400

    Updated file architecture for GraphQL refactor

--- server/src/routes/api/user-routes.ts
+++ server/src/routes/api/user-routes.ts
@@ -8,16 +8,43 @@ import {
   login,
 } from '../../controllers/user-controller.js';
 
-// import middleware
+// Import middleware for token authentication
 import { authenticateToken } from '../../services/auth.js';
 
-// put authMiddleware anywhere we need to send a token for verification of user
+/**
+ * Route: '/'
+ * Method: POST
+ * Description: Creates a new user in the database.
+ * Access: Public
+ * 
+ * Method: PUT
+ * Description: Saves a book to the logged-in user's account. Requires authentication.
+ * Access: Auth
+ */
 router.route('/').post(createUser).put(authenticateToken, saveBook);
 
+/**
+ * Route: '/login'
+ * Method: POST
+ * Description: Authenticates a user and returns a token for session management.
+ * Access: Public
+ */
 router.route('/login').post(login);
 
+/**
+ * Route: '/me'
+ * Method: GET
+ * Description: Retrieves the logged-in user's information, including saved books.
+ * Access: Auth
+ */
 router.route('/me').get(authenticateToken, getSingleUser);
 
+/**
+ * Route: '/books/:bookId'
+ * Method: DELETE
+ * Description: Deletes a saved book from the logged-in user's account by book ID.
+ * Access: Auth
+ */
 router.route('/books/:bookId').delete(authenticateToken, deleteBook);
 
 export default router;

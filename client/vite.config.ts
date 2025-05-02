9b439e1dbd959b84fa0cc1fd42345ea34dc43b8d
apoborka <alex.poborka@gmail.com>
2025-05-02 13:09:55 -0400

    Removed alias from vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,6 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@components': path.resolve(__dirname, './src/components'),
-    },
-  },
   build: {
     outDir: 'dist', // Default output directory for Vite builds
     emptyOutDir: true, // Ensure the directory is emptied before building
